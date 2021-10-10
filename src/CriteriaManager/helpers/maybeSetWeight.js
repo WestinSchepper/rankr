4279cf16a1b72de8ffcdd82056b889601a07848e
Westin Schepper <WestinSchepper@users.noreply.github.com>
2021-10-10 13:46:42 -0400

    Remove unused maybeSetWeight helper function

--- src/CriteriaManager/helpers/maybeSetWeight.js
+++
@@ -1,7 +0,0 @@
-import Big from 'big.js'
-
-function maybeSetWeight(criteria, criteriaCount) {
-  criteria.weight = criteria.weight ?? Big(1).div(criteriaCount).toNumber()
-}
-
-export default maybeSetWeight
