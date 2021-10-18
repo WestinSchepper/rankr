25b5d55e6c534c037302c9f9643026b8fe71774b
Westin Schepper <WestinSchepper@users.noreply.github.com>
2021-10-18 00:45:07 -0400

    Update sortScoredRecords to early return if config.sort is null

--- src/ScoreManager/helpers/sortScoredRecords.js
+++ src/ScoreManager/helpers/sortScoredRecords.js
@@ -1,4 +1,6 @@
 function sortScoredRecords(records, config) {
+  if (config.sort === null) return records
+
   const sortedRecords = records.sort((a, b) => {
     if (config.sort === 'desc') return b.score - a.score
     if (config.sort === 'asc') return a.score - b.score
